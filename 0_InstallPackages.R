81fed217284ec536a2ff4187cd61b3b9b935ba55
Stefan <steveg94@web.de>
2018-12-28 15:49:05 +0100

    major changes after review of journal, heavy changes to the ggplotting, integration of currency exchange rate

---
+++ 0_InstallPackages.R
@@ -0,0 +1,35 @@
+# 10 Data Import Preparation
+install.packages("openxlsx")
+install.packages("quantmod")
+
+# 20 Data Derivations
+
+# 30 Data Visualization
+install.packages("ggplot2")
+install.packages("tidyverse")
+install.packages("ggtern")
+install.packages("robustbase")
+
+# 40 Analysis
+install.packages("xtable")
+
+
+# 50 Optimization
+install.packages("foreach")
+install.packages("Rdonlp2", repos="http://R-Forge.R-project.org")
+install.packages("Rdonlp2", repos="https://svn.r-project.org/Rmetrics/trunk/Rdonlp2/")
+install.packages("doSNOW")
+
+install.packages("fPortfolio")
+install.packages("FRAPO")
+
+# 70 Visualization
+install.packages("PerformanceAnalytics")
+install.packages("tidyr")
+install.packages("PeerPerformance")
+
+# other
+install.packages("dplyr")
+
+# Markdown
+install.packages("FSelector")
